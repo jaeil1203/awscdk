a70fde26b0924c80e1b721cdaa530f3032a3b894
미디어프로세싱개발팀 김재일 <jaeilkim@sk.com>
2021-11-23 17:07:46 +0900

    change application name

--- bin/awscdk.ts
+++ bin/awscdk.ts
@@ -4,16 +4,26 @@ import * as cdk from '@aws-cdk/core';
 import { AppContext } from '../lib/app-context';
 import { VpcStack } from '../stack/vpc-stack';
 import { ResourceStack } from '../stack/resource-stack';
+import { TagEBSHandleStack } from '../stack/tag-ebs-volumn';
+import { AgentScheduleWorksHandleStack } from '../stack/agent-schedule-works-stack';
 
 const app = new cdk.App();
 const env = app.node.tryGetContext("env")==undefined?'dev':app.node.tryGetContext("env");
 
 AppContext.getInstance().initialize({
-  applicationName: 'myApp',
+  applicationName: 'skt',
   deployEnvironment: env
 });
 
 const vpcStack = new VpcStack(app, `VpcStack${env}`);
 const resourceStack = new ResourceStack(app, `ResourceStack${env}`, {
   vpc: vpcStack.vpc,
+});
+
+new TagEBSHandleStack(app, `TagEBSHandleStack${env}`, {
+  vpc: vpcStack.vpc,
+});
+
+new AgentScheduleWorksHandleStack(app, `AgentScheduleWorksHandleStack${env}`, {
+  vpc: vpcStack.vpc
 });
